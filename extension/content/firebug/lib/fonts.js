7c02b326e2fca6ca61aa1d61bebdd7287c353b0e
Florent FAYOLLE <florent.fayolle69@gmail.com>
2013-01-27 17:23:26 +0100

    revert previous modifications made on lib/fonts.js

--- extension/content/firebug/lib/fonts.js
+++ extension/content/firebug/lib/fonts.js
@@ -37,8 +37,9 @@ Fonts.getFonts = function(node)
     }
 
     var fontFaces = Dom.domUtils.getUsedFontFaces(range);
-    // converts fontFaces to an array:
-    var fonts = Array.slice(fontFaces);
+    var fonts = [];
+    for (var i=0; i<fontFaces.length; i++)
+        fonts.push(fontFaces.item(i));
 
     if (FBTrace.DBG_FONTS)
         FBTrace.sysout("Fonts.getFonts; used fonts", fonts);
